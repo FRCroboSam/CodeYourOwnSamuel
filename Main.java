6bab914a7368be6644b7005a639ce02d1c90085f
TslaRoadster2 <wobarber@outlook.com>
2023-05-17 10:30:42 -0700

    Made change for assigment
    This change made for the GitHub Project Stem assignment

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package CodeYourOwnSamuel;
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello World ");
+        System.out.println("This change was made by Will!");
 
     }
 }
